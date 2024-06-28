7f056b578f3548f5c36ef3ca3eb8ad2e7ef03afe
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-06-28 13:19:16 -0300

    Update exceptions.py

--- store/core/exceptions.py
+++ store/core/exceptions.py
@@ -1,13 +1,25 @@
 class BaseException(Exception):
+    """Exceção base para erros customizados."""
+    
     message: str = "Internal Server Error"
+    status_code: int = 500  # Código de status HTTP padrão
 
-    def __init__(self, message: str | None = None) -> None:
+    def __init__(self, message: str | None = None, status_code: int | None = None) -> None:
         if message:
             self.message = message
+        if status_code:
+            self.status_code = status_code
 
 
-class NotFoundException(BaseException):
-    message = "Not Found"
+class NotFound(BaseException):
+    """Exceção lançada quando um recurso não é encontrado."""
+
+    message: str = "Not Found"
+    status_code: int = 404
+
 
 class InsertError(BaseException):
-    message = "Erro ao inserir dados"
+    """Exceção lançada quando ocorre um erro ao inserir dados."""
+    
+    message: str = "Erro ao inserir dados"
+    status_code: int = 500  # Ou 422 para "Unprocessable Entity" (dados inválidos)
