6582d27f79852128933d799d53528929304c0514
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-07-01 16:09:32 -0300

    update decimal128

--- store/routers.py
+++ store/routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from store.controllers.product import router as product
+from bson import Decimal128
 
 api_router = APIRouter()
 api_router.include_router(product, prefix="/products")
