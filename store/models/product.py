a772ed19d2f9091c838ae3112de69318fab21385
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2024-07-01 15:50:59 -0300

    Update product - campo de preço

--- store/models/product.py
+++ store/models/product.py
@@ -1,6 +1,6 @@
 from store.models.base import CreateBaseModel
 from store.schemas.product import ProductIn
-
+from pydantic import Field
 
 class ProductModel(ProductIn, CreateBaseModel):
-    ...
+    price: float = Field(..., gt=0, description="Preço do produto")
